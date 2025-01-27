873f97b9d344e334eaf00c5c53d0836960a40c57
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2025-01-27 13:03:33 -0500

    feat: minor ui update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
   },
 });
 
