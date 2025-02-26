0a3f17a785397395fa393d3454552c00fdaa93d1
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2025-02-26 09:49:04 -0500

    feat: opacity

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -270,7 +270,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
   },
 });
 
