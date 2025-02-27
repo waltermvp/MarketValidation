80447bc4f0385b6d5fbeb31b6361d702896e6bf8
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2025-02-27 11:54:56 -0500

    feat: updated title font weight

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Animated from 'react-native-reanimated';
 
+import { LanguageSelector } from '@/components/language-selector';
 import { Text, View } from '@/components/ui';
 import { type Language } from '@/lib/i18n/resources';
-
-import { LanguageSelector } from './language-selector';
 
 type MenuBarProps = {
   lang: Language;
@@ -13,7 +12,7 @@ type MenuBarProps = {
 
 export function MenuBar({ lang, onLanguageChange }: MenuBarProps) {
   return (
-    <View className="align-center z-50 w-full flex-row items-center justify-between px-4  md:px-6">
+    <View className="align-center z-50 w-full flex-row items-center justify-between px-4 md:px-6">
       <Animated.View className={'left-4 top-4'}>
         <Text className="font-netflix-bold text-2xl !text-primary-500 sm:text-3xl">
           MapYourHealth
