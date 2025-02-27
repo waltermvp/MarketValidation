4249fee958a8910502aebad22f05e7d853d6572f
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2025-02-27 11:43:51 -0500

    feat: updated title responsive sizes

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -14,7 +14,7 @@ export function MenuBar({ lang, onLanguageChange }: MenuBarProps) {
   return (
     <View className="align-center z-50 w-full flex-row items-center justify-between px-4  md:px-6">
       <Animated.View className={'left-4 top-4'}>
-        <Text className="text-lg font-bold !text-primary-500 sm:text-4xl md:text-5xl">
+        <Text className="text-2xl font-bold !text-primary-500 sm:text-3xl">
           MapYourHealth
         </Text>
       </Animated.View>
